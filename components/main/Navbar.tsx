428541c88e871ed098a30c71ebb77a7754d459ba
Nava-stack <129419033+Nava-stack@users.noreply.github.com>
2024-08-27 16:36:54 +0530

    chore: Add Navbar component to layout and update page layout

---
+++ components/main/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-10 px-10">
+      <div className="w-full h-full flex flex-row items-center justify-between m-auto px"></div>
+    </div>
+  );
+};
+
+export default Navbar;
